9a2802b935033886e686cd9fd7a4b41cec1c22ab
Par-zy <j.davi4242@gmail.com>
2021-06-22 19:34:04 -0300

    fixed import problems

--- lib/shared/themes/app_text_styles.dart
+++ lib/shared/themes/app_text_styles.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/cupertino.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
